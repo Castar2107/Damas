325e8bc60c644ad997a747e0e4fad570731b595e
adam23ahernandez <adam23ahernandez@inslaferreria.cat>
2026-02-18 19:05:23 +0100

    Primer commit

---
+++ src/test/java/cat/inslaferreria/dames/DamesApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.inslaferreria.dames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
