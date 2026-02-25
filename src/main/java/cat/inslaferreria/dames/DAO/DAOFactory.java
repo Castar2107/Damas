11bf0bc497c40e79eee9e329a9c7b6f1d78eb0f5
Castar2107 <adam23icastarnado@inslaferreria.cat>
2026-02-25 18:48:01 +0100

    DAO

--- src/main/java/cat/inslaferreria/dames/DAO/DAOFactory.java
+++ src/main/java/cat/inslaferreria/dames/DAO/DAOFactory.java
@@ -1,5 +1,16 @@
 package cat.inslaferreria.dames.DAO;
 
 public class DAOFactory {
-
+	   if (instance == null) {
+		
+		             synchronized (DAOFactory.class) {
+		                 if (instance == null) {
+		                     instance = new DAOFactory();
+		                 }
+		             }
+		         }
+		         return instance;
+		     }
+public SociDAO getSociDAO () { 
+	return SociDAO ; }
 }
