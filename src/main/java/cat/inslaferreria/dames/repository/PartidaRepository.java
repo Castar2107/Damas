e6a34b508a3e54edf05b47235d4010a8f88ba8ed
adam23ahernandez <adam23ahernandez@inslaferreria.cat>
2026-02-25 19:53:27 +0100

    afegit findReservaByTaulaAndDataReserva

--- src/main/java/cat/inslaferreria/dames/repository/PartidaRepository.java
+++ src/main/java/cat/inslaferreria/dames/repository/PartidaRepository.java
@@ -1,11 +1,14 @@
 package cat.inslaferreria.dames.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import cat.inslaferreria.dames.model.Partida;
+import cat.inslaferreria.dames.model.Reserva;
 
 @Repository
 public interface PartidaRepository extends MongoRepository <Partida, String>{
-
+	Optional<Partida> findByReserva (Reserva reserva);
 }
--- src/main/java/cat/inslaferreria/dames/repository/ReservaRepository.java
+++ src/main/java/cat/inslaferreria/dames/repository/ReservaRepository.java
@@ -1,11 +1,15 @@
 package cat.inslaferreria.dames.repository;
 
+import java.util.Date;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import cat.inslaferreria.dames.model.Reserva;
+import cat.inslaferreria.dames.model.Taula;
 
 @Repository
 public interface ReservaRepository extends MongoRepository<Reserva, String> {
-
+	Optional<Reserva> findReservaByTaulaAndDataReserva(Taula taula, Date dataReserva);
 }
